3abb3c90f0f653ee11ee3c9babadb9e6ddc8459a
Darlen <tree317035791@163.com>
2016-08-22 09:00:57 +0800

    20160822  sqlserver D连接

--- src/test/darlen/crm/getrecord/TestGetLeadsRds.java
+++ src/test/darlen/crm/getrecord/TestGetLeadsRds.java
@@ -14,6 +14,8 @@ import org.apache.commons.httpclient.methods.PostMethod;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.Properties;
 
 /**
@@ -47,10 +49,27 @@ public class TestGetLeadsRds {
     private static String SCOPE ="crmapi";
     public static void main(String a[])
     {
-        getLeadsById("85333000000072001");
+//        getLeadsById("85333000000072001");
 //        getAllLeadsRds();
 //        updateRecords();
 //        readProperties();
+        testjdbc();
+    }
+
+    public static void testjdbc(){
+        String driverName = "com.microsoft.sqlserver.jdbc.SQLServerDriver";  //加载JDBC驱动
+        String dbURL = "jdbc:sqlserver://localhost:1433; DatabaseName=test";  //连接服务器和数据库test
+        String userName = "sa";  //默认用户名
+        String userPwd = "zaq1@WSX";  //密码
+        Connection dbConn;
+
+        try {
+            Class.forName(driverName);
+            dbConn = DriverManager.getConnection(dbURL, userName, userPwd);
+            System.out.println("Connection Successful!");  //如果连接成功 控制台输出Connection Successful!
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void readProperties(){
